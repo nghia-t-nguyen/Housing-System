f014db2bbc24410faf1e07d843cd1c99a7b6fbdd
deesort <54129589+deesort@users.noreply.github.com>
2020-10-21 22:07:37 -0400

    Updated StudentAccount.java
    Hardcoded, not sure if I put the non-hardcoded part correctly but it's commented out rn anyways - am I cycling through the listings and hosts correctly? Shouldn't they have a parameter of more than just host or listing? Or am I mistaken there. Please let me know and help <3

--- src/StudentAccount.java
+++ src/StudentAccount.java
@@ -1,39 +1,87 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StudentAccount extends Account{
-  private String studentID;
-  private ArrayList<Review> renterReviews;
-  private ArrayList<Listing> bookmarks;
-  private ArrayList<Listing> favoriteProperties;
+    private String studentID;
+    private ArrayList<Review> renterReviews;
+    private ArrayList<Listing> bookmarks;
+    private ArrayList<Listing> favoriteProperties;
 
-  StudentAccount(String username, String password, String firstName, String lastName,
-      String studentID) {
-    super(username, password, firstName, lastName);
-    this.studentID = studentID;
-    renterReviews = new ArrayList<Review>();
-    bookmarks = new ArrayList<Listing>();
-    favoriteProperties = new ArrayList<Listing>();
-  }
-  
-  public void addBookmark(Listing listing) {}
-  
-  public void addFavorite(Listing listing) {}
-  
-  public void addRenterReview(Review review) {}
-  
-  public void writeListingReview(Listing listing, int rating, String text) {}
-  
-  public void writeHostReview(HostAccount host, int rating, String text) {}
-  
-  public ArrayList<Review> getReviews() {
-    return renterReviews;
-  }
-  
-  public ArrayList<Listing> getBookmarks() {
-    return bookmarks;
-  }
-  
-  public ArrayList<Listing> getFavorites() {
-    return favoriteProperties;
-  }
+    StudentAccount(String username, String password, String firstName, String lastName,
+                   String studentID) {
+        super(username, password, firstName, lastName);
+        this.studentID = studentID;
+        renterReviews = new ArrayList<Review>();
+        bookmarks = new ArrayList<Listing>();
+        favoriteProperties = new ArrayList<Listing>();
+    }
+
+    public void addBookmark(Listing listing) {
+        bookmarks.add(listing);
+    }
+
+    public void addFavorite(Listing listing) {
+        favoriteProperties.add(listing);
+    }
+
+    public void addRenterReview(Review review) {
+        renterReviews.add(review);
+    }
+
+    public void writeListingReview(Listing listing, int rating, String text) {
+        Scanner key = new Scanner(System.in);
+        System.out.println("Write listing review");
+        String Lreview = key.nextLine();
+        System.out.println("Leave a rating from 1-5");
+        int LRating = key.nextInt();
+        while(LRating<1||LRating>5)
+        {
+            System.out.println("Invalid rating - out of range, try again. Ratings range from 1-5");
+            int NewLRating = key.nextInt();
+            NewLRating = LRating;
+        }
+        text = Lreview;
+        LRating = rating;
+        renterReviews[listing];
+        //I need to figure out how to add to a new listing each time it's called upon, I'm not sure this'll work but we'll see
+    }
+
+    /**
+     public void writeListingReview(Listing listing, int rating, String text) {
+     Review lreview = new Review(listing, rating, text);
+     }
+     **/
+
+    public void writeHostReview(HostAccount host, int rating, String text) {
+        Scanner key = new Scanner(System.in);
+        System.out.println("Write host review");
+        String Hreview = key.nextLine();
+        System.out.println("Leave a rating from 1-5");
+        int HRating = key.nextInt();
+        while(HRating<1||HRating>5)
+        {
+            System.out.println("Invalid rating - out of range, try again. Ratings range from 1-5");
+            int NewHRating = key.nextInt();
+            NewHRating = HRating;
+        }
+        text = Hreview;
+        HRating = rating;
+        renterReviews[host];
+    }
+/**
+    public void writeHostReview(HostAccount host, int rating, String text) {
+        Review hreview = new Review(host, rating, text);
+    }
+**/
+    public ArrayList<Review> getReviews() {
+        return renterReviews;
+    }
+
+    public ArrayList<Listing> getBookmarks() {
+        return bookmarks;
+    }
+
+    public ArrayList<Listing> getFavorites() {
+        return favoriteProperties;
+    }
 }
