b82de384095c1cb61b75eb3dec573e2a3739cfbf
Nghia Nguyen <nguyenn2001@gmail.com>
2020-10-24 22:52:33 -0400

    updating messaging system

--- src/MessageBox.java
+++ src/MessageBox.java
@@ -1,17 +1,31 @@
 import java.util.ArrayList;
 
 public class MessageBox {
-    private Account myUser;
-    private ArrayList<Message> messages;
-    private ArrayList<Account> contacts;
+  private Account myUser;
+  private ArrayList<String> messages;
 
-    public MessageBox(Account myUser){
-        this.myUser = myUser;
+  public MessageBox(Account myUser){
+    this.myUser = myUser;
+    messages = new ArrayList<String>();
+  }
+
+  public void sendMessage(Account to, String text) {
+    to.receiveMessage("From: " + myUser.toString() + "\n" +text);
+  }
+  
+  public void addMessage(String text){
+    messages.add(text);
+  }
+
+  public void showMessages(){
+    System.out.println("\nMessages:\n");
+    for (int i = messages.size(); i >= 0; i--) { //print most recent to oldest 
+      String message = (String) messages.get(i);
+      System.out.println(message + "\n_______________________");
     }
-
-    public void addMessage(String text){
-        messages.add(new Message(myUser,text));
-    }
-
-    public void showMessages(){}
+  }
+  
+  public ArrayList<String> getMessages() {
+    return messages;
+  }
 }
