d7bd86bbcac389179a13dfe6ffa3559070aa2adb
deesort <ortunodylan@gmail.com>
2020-11-02 17:22:29 -0500

    Update Lease.java
    One printToFIle, every statement made so that it's Lease+="blah blah example...." ; now.

--- src/Lease.java
+++ src/Lease.java
@@ -1,153 +1,123 @@
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * The abstract class Lease contains the private String lease which is adjusted
- * throughout the entire class and then returned. 
- * Created by Team QUEBEC
- */
-public abstract class Lease {
-	private String Lease;
+public class Lease {
+    
+    private String Lease;
 
-	/**
-	 * The Lease constructor takes in an ArrayList of StudentAccount objects, a
-	 * HostAccount object, and a Listing object. It takes the lease String private
-	 * variable and adds the first and last name Strings of the StudentAccounts
-	 * within the ArrayList, it then adds the same from the HostAccount and finally,
-	 * it adds to toString for the Listing. 
-	 * Created by Team QUEBEC
-	 */
-	public Lease(ArrayList<StudentAccount> tenants, HostAccount landlord, Listing listing) {
+    /**
+     * Class Lease is meant to generate the text of the lease in a .txt file. It imports BufferedWriter, FileWriter,
+     * IOException, and ArrayList to do so.
+     */
+    
+    public Lease(ArrayList<StudentAccount> tenants, HostAccount landlord, Listing listing){
 
-		Lease = "Tenants: ";
-		for (StudentAccount temp : tenants) {
-			Lease += temp.getFirstName() + " " + temp.getLastName() + "\n";
-		}
-		Lease += "\n Landlord: " + landlord.getFirstName() + " " + landlord.getLastName() + "\n Listing: "
-				+ listing.toString();
-	}
+        Lease = "Tenants: ";
+        for(StudentAccount temp : tenants ) {
+            Lease += temp.getFirstName() + " " + temp.getLastName() + "\n";
+        }
+        Lease += "\n Landlord: " + landlord.getFirstName() + " " + landlord.getLastName() + "\n Listing: " +
+                listing.toString();
+    }
 
-	/**
-	 * The addDuration method takes in several Strings and uses Filewriter to write
-	 * a String with the passed Strings in it to a txt file. 
-	 * Created by Team QUEBEC
-	 */
-	public void addDuration(String start, String tenants, String host) {
-		try (FileWriter file = new FileWriter("src/lease.txt")) {
-			file.write("This lease agreement is made and enterd on " + start + " by and between" + host + "and "
-					+ tenants + "\n");
-			file.flush();
-		} catch (IOException e) {
+    /**
+     * This is the constructor for the Lease class
+     * @param start
+     * @param tenants
+     * @param host
+     */
+    public void addDuration(String start, String tenants, String host){
+            Lease+="This lease agreement is made and entered on "+start+" by and between" + host + "and "+tenants+"\n";
+    }
 
-		}
-	}
+    /**
+     * addDuration displays the durations of the lease, using String start, String tenants, and String host
+     */
 
-	/**
-	 * The addTenantAct method uses FileWriter to write a String to a txt file.
-	 * Created by Team QUEBEC
-	 */
-	public void addTenantAct() {
-		try (FileWriter file = new FileWriter("src/lease.txt")) {
-			file.write(
-					"1. Landloard Tenant Act. This Rental Agreement is governed by the South Carolina Residential Landlord and Tenant Act.\n");
-			file.flush();
-		} catch (IOException e) {
+    public void addTenantAct()
+    {
 
-		}
-	}
+          Lease+="1. Landlord Tenant Act. This Rental Agreement is governed by the South Carolina Residential Landlord and Tenant Act.\n";
+    }
 
-	/**
-	 * The Property method uses FileWriter to write a String to a txt file while
-	 * taking in 3 ints to be part of it. 
-	 * Created by Team QUEBEC
-	 */
-	public void Property(int bedrooms, int bathrooms, String address, int zip) {
-		try (FileWriter file = new FileWriter("src/lease.txt")) {
-			file.write(
-					"2. Property. Landlord, in consideration of the lease payments provided in this agreement, leases to Tenant a house with "
-							+ bedrooms + " bedrooms and " + bathrooms + "bathrooms, located at " + address
-							+ ", South Carolina " + zip
-							+ ". No other portion of the building wherein the Property is located is included unless expressly provided for in this agreement.\n");
-			file.flush();
-		} catch (IOException e) {
+    /**
+     * Add tenant adds a line regarding the LandLord tenant act. 
+     */
+    
+    public void Property(int bedrooms, int bathrooms, String address, int zip)
+    {
 
-		}
-	}
+            Lease+="2. Property. Landlord, in consideration of the lease payments provided in this agreement, leases to Tenant a house with "+bedrooms+" bedrooms and "+bathrooms+ "bathrooms, located at "+address+", South Carolina "+zip+". No other portion of the building wherein the Property is located is included unless expressly provided for in this agreement.\n";
 
-	/**
-	 * The PaymentTerm method uses FileWriter to write a String to a txt file while
-	 * taking in two doubles to be part of it. 
-	 * Created by Team QUEBEC
-	 */
-	public void PaymentTerm(double beg, double end) {
-		try (FileWriter file = new FileWriter("src/lease.txt")) {
-			file.write("3. Term. The Tenant will coninue to pay rent from" + beg + "to " + end + ".\n");
-			file.flush();
-		} catch (IOException e) {
+    }
 
-		}
-	}
+    /**
+     * Property displays a line regarding property in the listing, using int bedrooms, int bathrooms, String address, and int zip.
+     */
+    
+    public void PaymentTerm(double beg, double end){
+        Lease+="3. Term. The Tenant will coninue to pay rent from"+beg+ "to "+end+".\n";
+    }
 
-	/**
-	 * The Payment method uses FileWriter to write a String to a txt file while
-	 * taking in a double to be part of it. 
-	 * Created by Team QUEBEC
-	 */
-	public void Payment(double payment) {
-		try (FileWriter file = new FileWriter("src/lease.txt")) {
-			file.write("4. Rent. The Tenant will pay " + payment + " each month on the first of the month.\n");
-			file.flush();
-		} catch (IOException e) {
+    /**
+     * PaymentTerm displays a line regarding when the tenant should pay using double beg and double end
+     */
+    
+    public void Payment(double payment)
+    {
+        Lease+="4. Rent. The Tenant will pay "+payment+" each month on the first of the month.\n";
+    }
 
-		}
-	}
+    /**
+     * Payment displays the amount to be payed each month using double payment
+     */
+    
+    public void PaymentAddress(String address)
+    {
+        Lease += "5. Payment should be sent to" + address + ".\n";
+    }
 
-	/**
-	 * The PaymentAddress method uses FileWriter to write a String to a txt file
-	 * while taking in a String to be part of it. 
-	 * Created by Team QUEBEC
-	 */
-	public void PaymentAddress(String address) {
-		try (FileWriter file = new FileWriter("src/lease.txt")) {
-			file.write("5. Payment should be sent to" + address + ".\n");
-			file.flush();
-		} catch (IOException e) {
+    /**
+     * PaymentAddress displays a line regarding where payments should be sent using String address
+     */
+    
+    public void Damages(double cost){
+        Lease+="6. Damages. Charges will be billed to the client for damaged property, up to "+cost+".\n";
+    }
 
-		}
-	}
+    /**
+     * Damages displays a line regarding bills for damages using double cost
+     */
+    
+    public void Signatures(String tenant, String host)
+    {
+        Lease+=""+tenant+"\n------------------"+"\n"+host+"\n------------------";
+    }
 
-	/**
-	 * The Damages method uses FileWriter to write a String to a txt file while
-	 * taking in a double to be part of it. 
-	 * Created by Team QUEBEC
-	 */
-	public void Damages(double cost) {
-		try (FileWriter file = new FileWriter("src/lease.txt")) {
-			file.write("6. Damages. Charges will be billed to the client for damaged property, up to " + cost + ".\n");
-			file.flush();
-		} catch (IOException e) {
+    /**
+     * Signatures displays the signatures of the tenant and host, using String tenant and String host
+     */
+    
+    public String toString(){
+        return Lease;
+    }
 
-		}
-	}
+    /**
+     * toString returns Lease of type String
+     */
+    
+    public void printToFile(String fileName)
+            throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Lease.txt"));
+        writer.write(Lease);
 
-	/**
-	 * The Signatures method takes in two Strings and uses them to create a String
-	 * and write them to a file using FileWriter. 
-	 * Created by Team QUEBEC
-	 */
-	public void Signatures(String tenant, String host) {
-		try (FileWriter file = new FileWriter("src/lease.txt")) {
-			file.write(tenant + "\n------------------" + "\n" + host + "\n------------------");
-			file.flush();
-		} catch (IOException e) {
+        writer.close();
+    }
 
-		}
-	}
-	/*
-	 * public void printToFile(String fileName) throws IOException { BufferedWriter
-	 * writer = new BufferedWriter(new FileWriter("Lease.txt"));
-	 * writer.write(Lease); writer.close(); } We don't need this part anymore,
-	 * unless I'm mistaken - dylan
-	 */
+    /**
+     * printToFile makes use of BufferedWriter, FIleWriter, and IOException to write the lines from Lease to a file titled Lease.txt
+     */
+
 }
