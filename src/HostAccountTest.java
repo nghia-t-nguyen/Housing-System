39f860e54ef55fc03dd573f61d619741e67b1847
Rhylen 'Jennifer' Nguyen <jtn1@email.sc.edu>
2020-11-15 22:50:46 -0500

    Update HostAccountTest.java

--- src/HostAccountTest.java
+++ src/HostAccountTest.java
@@ -2,28 +2,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
-import org.junit.AfterEach;
-import org.junit.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HostAccountTest {
-	private ArrayList<Listing> listing = new ArrayList<Listing>;
+	private ArrayList<Listing> listing = new ArrayList<Listing>();
 		
 		@BeforeEach
 		public static void setup() {
 			HostAccount hostAccount = new HostAccount(null, 0, null, null);
-			ArrayList<Listing> listing = new ArrayList<Listing>;
+			ArrayList<Listing> listing = new ArrayList<Listing>();
 		}
 		
 		@AfterEach
-		public static void tearDown() {
+		public void tearDown() {
 			listing.clear();
 		}
 
 		@Test
 		public void testWriteRenterReview() {
 			StudentAccount subject = new StudentAccount(null, 0, null, null, null);
-			subject.addAccountReview(new Review((null, 0, null)));
+			subject.addAccountReview(new Review(null, 0, null));
 		}
 		
 		@Test
@@ -31,4 +33,5 @@ public class HostAccountTest {
 			listing.add(null);
 		}
 		
-} 
+
+}
